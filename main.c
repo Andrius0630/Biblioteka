647b191ad5be5abb721afcfa842185ae18e5c9b0
Andrius0630 <kolendarik123@gmail.com>
2025-01-07 14:29:25 +0200

    update

--- main.c
+++ main.c
@@ -3,29 +3,7 @@
 #include "booksStruct.h"
 
 int main(void) {
-    /*
-    char *filename = NULL;
-    enterFileName(&filename);
-    */
-
-    
-    
-    
-    /*createFileAuthor(books, lineCount);*/
-
     renderLogIn();
-
-
-
-
-
-    
-    
-    /*nufree(filenameUserData);
-    free(filenamePasswd);*/
-
-
-    printf("Done!\n");
     return 0;
 }
 
